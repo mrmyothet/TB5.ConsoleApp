80a9471b81517a042267e1dc52a3980837b4c2db
MyoThet <mr.myothet@gmail.com>
2026-03-11 00:05:34 +0700

    AdoDotNet Sample

---
+++ TB5.ConsoleApp/AdoDotNetSample/AdoDotNetService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TB5.ConsoleApp.AdoDotNetSample
+{
+    public class AdoDotNetService
+    {
+
+        // fields
+        string connectionString = @"Data Source=MYOTHETPC\\MSSQLEXPRESS2022;Initial Catelog = Batch5MiniPOS; User ID = sa; Password=admin123!;TrustedServerCertificate=True";
+
+        public void Create()
+        {
+            string query = @"
+                INSERT INTO [dbo].[Tbl_Product]
+                       ([ProductCode]
+                       ,[ProductName]
+                       ,[Price])
+                 VALUES
+                       (@ProductCode
+                       ,@ProductName
+                       ,@Price)";
+
+            SqlConnection connection = new SqlConnection();
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ProductCode", "P001");
+            cmd.Parameters.AddWithValue("@ProductName", "Strawberry");
+            cmd.Parameters.AddWithValue("@Price", 500);
+
+            int result = cmd.ExecuteNonQuery();
+
+
+
+
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Edit()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+        }   
+    }
+
+    public class Tbl_Product
+    {
+        public int Id { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public class Tbl_Sale
+    {
+        public int SaleId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime? SaleDate { get; set; }
+    }
+}
--- TB5.ConsoleApp/Program.cs
+++ TB5.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Waht is you name? ");
+//string name = Console.ReadLine();
+
+//Console.WriteLine($"Hello," + name);
+
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Data Source=MYOTHETPC\\MSSQLEXPRESS2022;Initial Catelog=Batch5MiniPOS;User ID=sa;Password=admin123!;TrustedServerCertificate=True");
+connection.Open();  
+
+Console.ReadLine();
+
